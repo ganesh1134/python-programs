2380ff4ac7a0a78598e32f058802d459cdc5d421
ganesh1134 <magantiganeshraju30@gmail.com>
2025-03-11 13:34:54 +0530

    Add files via upload

---
+++ ganesh.py
@@ -0,0 +1,16 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("hello world")
+hello world
+>>> print(ganesh)
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    print(ganesh)
+NameError: name 'ganesh' is not defined
+>>> print(ganesh)
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    print(ganesh)
+NameError: name 'ganesh' is not defined
+>>> print("ganesh")
+ganesh
